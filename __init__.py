efb40090ced103c2aac04e7625245675025143f6
Bernat Brunet <bernat@nan-tic.com>
2023-11-03 11:36:26 +0100

    Create the models to generat the 111 report in Spanish account.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,19 @@
-# This file is part aeat_111 module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
 from trytond.pool import Pool
+from . import aeat
+
 
 def register():
     Pool.register(
+        aeat.Report,
+        aeat.TemplateTaxCodeMapping,
+        aeat.TemplateTaxCodeRelation,
+        aeat.TaxCodeMapping,
+        aeat.TaxCodeRelation,
         module='aeat_111', type_='model')
     Pool.register(
+        aeat.CreateChart,
+        aeat.UpdateChart,
         module='aeat_111', type_='wizard')
-    Pool.register(
-        module='aeat_111', type_='report')
