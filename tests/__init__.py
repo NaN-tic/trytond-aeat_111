5a92a7bf09ab70be6307dd94636c1e108548833c
Bernat Brunet <bernat@nan-tic.com>
2023-11-03 11:49:45 +0100

    Fix test call.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,3 @@
 # This file is part aeat_111 module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from .test_aeat_111 import suite
-
-__all__ = ['suite']
